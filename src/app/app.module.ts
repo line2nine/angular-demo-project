70f30dcdb74a8240eb4429d518cf259c8a9074ee
line2nine <hailinh203@gmail.com>
2020-06-18 17:49:32 +0700

    Add color picker and calculator project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
 import { PetInfoComponent } from './pet-info/pet-info.component';
+import { ColorPickerComponent } from './color-picker/color-picker.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FontSizeEditorComponent,
     PetInfoComponent,
+    ColorPickerComponent,
+    CalculatorComponent,
   ],
   imports: [
     BrowserModule,
